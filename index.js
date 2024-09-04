bb713c2a1fc69296cee79716c4195890c2d58134
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-09-04 07:32:04 +0100

    Data type & Operator

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-// let start withe the cours
-console.log('Hello World');
+// Data type : JavaScript
+console.log("-----------------------------------Data type---------------------------");
+let name = "John";
+let age = 25;
+let isActive = true;
+let user={name:"chebicheb", age:25, isActive:true};
+let nember=[1,2,3,4,5];
+
+// consloe the type of data
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isActive);
+console.log(typeof user);
+console.log(typeof nember);
+// Operator
+console.log("---------------------------Operator--------------------");
+let sum= 10+20;
+let diffrenrce= 20-10;
+let product= 10*20;
+let division= 20/10;
+let remainder= 20%10;
+
+console.log(sum);
+console.log(diffrenrce);
+console.log(product);
+console.log(division);
+console.log(remainder);
