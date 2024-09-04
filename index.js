ed66d17ae131c71e1da55f3e0032c44cb43b712a
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-09-04 12:13:24 +0100

    Function and scope

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// Data type : JavaScript
-console.log("-----------------------------------Data type---------------------------");
-let name = "John";
-let age = 25;
-let isActive = true;
-let user={name:"chebicheb", age:25, isActive:true};
-let nember=[1,2,3,4,5];
+// function and scope Exampoles 
+const sun= function(a,b){
+    return a+b;
+}
 
-// consloe the type of data
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isActive);
-console.log(typeof user);
-console.log(typeof nember);
-// Operator
-console.log("---------------------------Operator--------------------");
-let sum= 10+20;
-let diffrenrce= 20-10;
-let product= 10*20;
-let division= 20/10;
-let remainder= 20%10;
+console.log(sun(2,3));
 
-console.log(sum);
-console.log(diffrenrce);
-console.log(product);
-console.log(division);
-console.log(remainder);
+// this is multply function
+const multiply = (a,b)=>a*b;
+console.log(multiply(2,3));
+
+// local scope and global scope
+console.log('local scope and global scope');
+
+let globalVar = 'This is global variable';
+
+function scopeChack(){
+    let localVar = 'This is local variable';
+    console.log(globalVar);
+    console.log(localVar);
+    console.log('End of scopeChack function');
+}
+scopeChack();
+// console.log(globalVar);
+// console.log(localVar); // this will give error because localVar is local variable
